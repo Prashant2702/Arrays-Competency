e612d4679969c3aefc2b61f3ddcd664d85efd152
Prashant2702 <109785759+Prashant2702@users.noreply.github.com>
2023-01-18 18:19:53 +0530

    Add files via upload

--- array02.cpp
+++ array02.cpp
@@ -1,3 +1,24 @@
-...Sum of all Subarrays...
-given an array a[] of size n.output sum of each sub array of the given array.
-In these program we are going to print out the sum of the sub arrays by simply using the for loops.
+//Sum of all Subarrays.
+// given an array a[] of size n.output sum of each sub array of the given array.
+#include<iostream>
+using namespace std;
+
+int main(){
+int n;
+cin>>n;
+
+int a[n];
+for(int i=0;i<n;i++){
+    cin>>a[i];
+    }
+
+    int curr=0;
+for(int i=0;i<n;i++){
+    curr=0;
+    for(int j=1;j<n;j++){
+        curr += a[j];
+        cout<<curr<<endl;
+    }
+}
+    return 0;
+}
