bb74d797c720ab411a7bb2a3f1b4868acc441420
Prashant2702 <109785759+Prashant2702@users.noreply.github.com>
2023-01-28 14:28:48 +0530

    Character Arrays.

---
+++ largestWord.cpp
@@ -0,0 +1,34 @@
+//Finding largest word in a sentence.
+#include<iostream>
+using namespace std;
+
+int main(){
+int n;
+cin>>n;
+cin.ignore();
+char  arr[n+1];
+cin.getline(arr,n);//stores the complete sentence including the spaces.
+cin.ignore();//Remove the buffer.
+
+int i=0;
+int currLen = 0, maxLen = 0;
+
+while(1)
+{
+    if(arr[i] == ' ' || arr[i] == '\0'){
+
+        if(currLen > maxLen){
+            maxLen = currLen;
+        }
+        currLen = 0;
+    }
+    else
+currLen++;
+if(arr[i] == '\0')
+break;
+
+i++;
+}
+cout<< maxLen <<endl;
+    return 0;
+}
